fea397f63843fb6b0b0a8094b08485bfd06e0035
visheshbaghel <vishesh.baghel@batonsystems.com>
2023-02-25 23:11:03 +0530

    feat: added profiles for dev, prod and qa

--- src/main/java/com/example/springboot/tutorial/controller/HelloController.java
+++ src/main/java/com/example/springboot/tutorial/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.example.springboot.tutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String hello() {
-        return "Hello World! from spring boot";
+        return welcomeMessage;
     }
 }
